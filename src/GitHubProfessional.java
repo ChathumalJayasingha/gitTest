05f34f15e53935f65d77e352fb75c7e31ac2871a
chathumal <chathumaljayasingha@hotmail.coim>
2021-10-08 18:51:08 +0530

    Initial commit

---
+++ src/GitHubProfessional.java
@@ -0,0 +1,10 @@
+public class GitHubProfessional {
+    public static void main(String[] args) {
+        System.out.println("Chathumal");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
